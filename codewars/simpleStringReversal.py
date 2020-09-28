9f4f9f70c74fa10b68fffd62094dfcdbeed8e253
lsudupe <sudupelaura@gmail.com>
2020-09-28 19:04:29 +0200

    kata suscesfully finished

--- codewars/simpleStringReversal.py
+++ codewars/simpleStringReversal.py
@@ -2,17 +2,29 @@
 string = 'Alex mat tie'
 
 def solve(word):
-  replace_string = word.replace(' ', '$')
-  replace_list = replace_string.split()
-  spacePosition = []
-  for i, elem in enumerate(replace_list):
-    if '$' in elem:
-      spacePosition.append(i)
-
+  #el primer paso de la función es darle la vuelta al string.Check
   reverse = ''
+  reverse_nospace = []
   for char in range(len(word)-1, -1, -1):
     reverse += word[char] 
+  for i in reverse:
+    reverse_nospace.append(i)
 
-  return spacePosition
+  #el segundo paso buscar el index de los espacios, usare list comprehension [expression for item in list]
+  char_list = []
+  index = []
+  for char in word:
+    char_list.append(char)
+  for idx, space in enumerate(char_list):
+    if space == ' ':
+      index.append(idx)
+
+  #el tercero, hacer que coincidan los espacios
+    final_string = ''
+    for position in index:
+      reverse_nospace.insert(position, ' ')
+    final_string = ''.join(reverse_nospace)
+    return final_string
 
 print(solve(string))
+
